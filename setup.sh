d0ee9b82d02bd839785a080eed18955ddeaaafb2
Erik Knudsen <31932266+erik1066@users.noreply.github.com>
2020-09-18 10:00:35 -0400

    Updated scripts.sh to include GitHub CLI tools installation

--- setup.sh
+++ setup.sh
@@ -108,3 +108,11 @@ sudo usermod -aG docker $USER
 
 ### Alternative postman install from Flathub
 sudo flatpak install flathub com.getpostman.Postman
+
+# ------------------------------------
+# Install GitHub CLI tools:
+# ------------------------------------
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+sudo apt-add-repository https://cli.github.com/packages
+sudo apt update
+sudo apt install gh
